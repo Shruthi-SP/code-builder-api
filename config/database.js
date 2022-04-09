9b1eb9e38b04992051fbaccdaf91b156bac121b0
Shruthi-SP <shrupr@gmail.com>
2022-04-09 13:14:32 +0530

    added path package

--- config/database.js
+++ config/database.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose')
 
-const configureDB = ()=>{
-    mongoose.connect('mongodb://localhost:27017/dec15-2021')
-        .then(()=>{
+mongoose.Promise = global.Promise
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/code-builder'
+const configureDB = () => {
+    mongoose.connect(CONNECTION_URI, { useNewUrlParser: true })
+        .then(() => {
             console.log('connected to db')
         })
-        .catch((err)=>{
+        .catch((err) => {
             console.log('err in connecting to db')
         })
 }
 
 module.exports = configureDB
+
+// const mongoose = require('mongoose')
+
+// const configureDB = ()=>{
+//     mongoose.connect('mongodb://localhost:27017/dec15-2021')
+//         .then(()=>{
+//             console.log('connected to db')
+//         })
+//         .catch((err)=>{
+//             console.log('err in connecting to db')
+//         })
+// }
+
+// module.exports = configureDB
--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const configureDB = require('./config/database')
 const router = require('./config/routes')
 
 const app = express()
-const port = 3044
+const port = process.env.PORT || 3044
+const path = require('path')
 
 configureDB()
 app.use(cors()) 
 app.use(express.json())
 app.use(router)
+app.use(express.static(path.join(__dirname, 'client/build')))
+app.get('*',(req, res) => {
+    res.sendFile(path.join(__dirname+'client/build/index.html'))
+})
 
 app.listen(port, ()=>{
     console.log('server running on port', port)
